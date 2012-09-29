e7cc938508d8681c383821d9a7967deca7e05fec
blueskywalker <jerrykskim@gmail.com>
2012-09-28 18:27:29 -0700

    add new analyzer

--- src/test/java/org/blueskywalker/korean/analyzer/analyzertest/AnalyzerTest.java
+++ src/test/java/org/blueskywalker/korean/analyzer/analyzertest/AnalyzerTest.java
@@ -3,7 +3,13 @@ package org.blueskywalker.korean.analyzer.analyzertest;
 import com.ibm.icu.impl.ICUConfig;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.lucene.analysis.icu.segmentation.ICUTokenizer;
+import org.apache.lucene.analysis.standard.StandardTokenizer;
+import org.apache.lucene.util.AttributeImpl;
+import org.apache.lucene.util.Version;
 import org.junit.Test;
 
 /**
@@ -19,7 +25,7 @@ public class AnalyzerTest {
 
         String field = "test";
         String source = "너무 너무beautiful한 정서적인나무가 아닌가 싶네요.";
-         source = "Once you have tests, you'll want to run them.";
+        source = "Once you have tests, you'll want to run them.";
 
 
         TestAnalyzer[] analyzers = {
@@ -45,4 +51,27 @@ public class AnalyzerTest {
 
     }
 
+    @Test
+    public void testTokenizer() {
+
+        String test = "한글이 tokenizing 되어서 나오면 좋지 I am hello.";
+        
+        StandardTokenizer tokenizer = new StandardTokenizer(Version.LUCENE_36, new StringReader(test));
+        try {
+
+            while (tokenizer.incrementToken()) {
+                if (tokenizer.hasAttributes()) {
+                    Iterator<AttributeImpl> iter = tokenizer.getAttributeImplsIterator();
+                    while (iter.hasNext()) {
+                        AttributeImpl attr = iter.next();
+                       // System.out.println(attr.getClass().getName());
+                        System.out.println(attr.toString());
+                    }
+                }
+
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(AnalyzerTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
